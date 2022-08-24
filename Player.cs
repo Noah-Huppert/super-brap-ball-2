0d80f385dae2888de12e35e0a8dcf24084072876
Noah Huppert <3323504+Noah-Huppert@users.noreply.github.com>
2022-08-23 22:51:44 -0400

    Made player camera change based on floor angle

--- Player.cs
+++ Player.cs
@@ -1,16 +1,23 @@
 using Godot;
 using System;
 
+/// Player controller.
+/// Main responsibilities: moving camera with player
 public class Player : Spatial
 {
-	private RigidBody collision;
+	/// The player collision node
+	private PlayerCollision collision;
+
+	/// The spring arm which the camera is mounted onto
 	private SpringArm camera;
+
+	/// The position the camera was positioned via the 3D editor when the scene started, used to position the camera in a good position above the player.
 	private Vector3 initialCameraOffset;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		this.collision = GetNode<RigidBody>("Collision");
+		this.collision = GetNode<PlayerCollision>("Collision");
 		this.camera = GetNode<SpringArm>("SpringArm");
 
 		this.initialCameraOffset = this.camera.Transform.origin;
@@ -19,8 +26,16 @@ public class Player : Spatial
 	 // Called every frame. 'delta' is the elapsed time since the previous frame.
 	 public override void _Process(float delta)
 	 {
+		// Move camera with player
 		var cameraTransform = this.camera.Transform;
 		cameraTransform.origin = this.collision.Transform.origin + this.initialCameraOffset;
+
+		// Angle camera based on ground
+		if (this.collision.floorCollisionNormal != null)
+		{
+			cameraTransform = cameraTransform.Rotated(new Vector3(1, 0, 0), cameraTransform.basis.GetEuler().x - (this.collision.floorCollisionNormal.Value.z * -1));
+		}
+		
 		this.camera.Transform = cameraTransform;
 	}
 }
--- PlayerCollision.cs
+++ PlayerCollision.cs
@@ -2,13 +2,21 @@ using Godot;
 
 public class PlayerCollision : RigidBody
 {
+	public Vector3? floorCollisionNormal = null;
+
 	public override void _IntegrateForces(PhysicsDirectBodyState state)
 	{
 		this.Sleeping = false;
 	   
+	   // Move player
 		var forwardStrength = Input.GetActionStrength("player_forward") - Input.GetActionStrength("player_backward");
 		var leftStrength = Input.GetActionStrength("player_left") - Input.GetActionStrength("player_right");
 	   
 		state.AddCentralForce(new Vector3(leftStrength, 0, forwardStrength));
+
+		// Detect collision with floor
+		if (state.GetContactCount() > 0) {
+			this.floorCollisionNormal = state.GetContactLocalNormal(0);
+		}
 	}
 }
