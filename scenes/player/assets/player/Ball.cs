04ceb3c7a5d36b76e8444ea67e66420aba4c09e5
Noah Huppert <3323504+Noah-Huppert@users.noreply.github.com>
2022-09-05 02:47:31 -0400

    made ball not rotate when flying

--- scenes/player/assets/player/Ball.cs
+++ scenes/player/assets/player/Ball.cs
@@ -3,21 +3,32 @@ using System;
 
 public class Ball : Spatial
 {
+    // Animations on ball.
     private AnimationPlayer animationPlayer;
+
+    // If the ball is open.
+    public bool isOpen;
+
+    // The rotation the ball started in.
+    private Vector3 startingGlobalRotation;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         this.animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        this.startingGlobalRotation = this.GlobalRotation;
     }
 
     public void Open() {
         this.animationPlayer.Play("Open");
+        this.isOpen = true;
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    
+    public override void _Process(float delta)
+    {
+        if (this.isOpen) {
+            this.GlobalRotation = this.startingGlobalRotation;
+        }
+    }
 }
