90e2ea0e7095644b58ebc2152a96f5a20c3e89f2
Noah Huppert <contact@noahh.io>
2022-09-07 08:47:01 -0400

    fixed DebugVector3

--- scenes/debug/vector3/scripts/DebugVector3.cs
+++ scenes/debug/vector3/scripts/DebugVector3.cs
@@ -19,10 +19,19 @@ public class DebugVector3 : Spatial
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta)
 	{
-		var normalizedVector = this.vector.Normalized() + new Vector3(0.1f, 0.1f, 0.1f);
+        var nVec = this.vector.Normalized();
 
-        //this.renderArrow.GlobalRotation = this.renderArrow.GlobalRotation.DirectionTo(normalizedVector);
-        this.renderArrow.Scale = normalizedVector;
+		// Set angle
+		var dirVec = Vector3.Zero.DirectionTo(nVec);
+        var rightAngle = (Mathf.Pi / 2);
+		
+        this.renderArrow.GlobalRotation = new Vector3(
+			dirVec.z != 0 ? rightAngle / dirVec.z : 0,
+			0,
+			dirVec.x != 0 ? rightAngle / dirVec.x : 0
+		);
 
+		// Set scale
+        this.renderArrow.Scale = new Vector3(0.05f, nVec.Length(), 0.05f);
     }
 }
