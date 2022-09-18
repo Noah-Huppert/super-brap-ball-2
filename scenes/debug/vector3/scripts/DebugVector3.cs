e9edad5fa083a3a61110d2493ca77b2c1bb7fb35
Noah Huppert <contact@noahh.io>
2022-09-18 16:26:14 -0400

    Revert "wip making a 2d draw version of debugvector3"
    This reverts commit 01754205813e876ca9cd7034787f9dc66933c72b.

--- scenes/debug/vector3/scripts/DebugVector3.cs
+++ scenes/debug/vector3/scripts/DebugVector3.cs
@@ -1,207 +1,207 @@
 using Godot;
 using System;
 
-public class DebugVector3 : Spatial
+public class DebugVector3 : ImmediateGeometry
 {
-	// The label shown next to the vector.
-	[Export]
-	public String label = "";
+    // The label shown next to the vector.
+    [Export]
+    public String label = "";
 
-	// The vector which will be represented by the render.
-	[Export]
-	public Vector3 vector = Vector3.Zero;
+    // The vector which will be represented by the render.
+    [Export]
+    public Vector3 vector = Vector3.Zero;
 
-	// Gets and sets the vector.x property.
+    // Gets and sets the vector.x property.
 	[Export]
 	public float vectorX
-	{
-		get
-		{
-			return this.vector.x;
-		}
-		set
-		{
-			this.vector.x = value;
-		}
-	}
-
-	// Gets and sets the vector.y property.
-	[Export]
-	public float vectorY
-	{
-		get
-		{
-			return this.vector.y;
-		}
-		set
-		{
-			this.vector.y = value;
-		}
-	}
-
-	// Gets and sets the vector.z property.
-	[Export]
-	public float vectorZ
-	{
-		get
-		{
-			return this.vector.z;
-		}
-		set
-		{
-			this.vector.z = value;
-		}
-	}
-
-	// Color in which vector body will be drawn.
-	[Export]
-	public Color bodyColor = new Color(1, 0, 0, 1);
-
-	// Color in which vector end "arrow" will be drawn.
-	[Export]
-	public Color arrowColor = new Color(0, 1, 0, 1);
-
-	// If true the vector's value will be shown in the label.
-	[Export]
-	public bool includeValueInLabel = true;
-
-	// If greater than 0 the vector will be rounded to this number of decimal places when shown in the label.
-	[Export]
-	public int roundLabelValue = 2;
-
-	// This number will be used to scale the vector so it can appear bigger or smaller on the screen. The vector value shown in the label will not be scaled by this. To indicate the vector is being scaled an astrix will be appended.
-	[Export]
-	public float vectorScale = 1;
-
-	private DebugVector3Viewport viewport;
-    private Spatial spatial;
-
-    // Create a DebugVector3 scene and add it as a child of the parent node.
-    static public DebugVector3 AddNode(Node parent, Vector3 initVec, String label, bool includeValueInLabel = true)
-	{
-		var scene = ResourceLoader.Load<PackedScene>("res://scenes/debug/vector3/DebugVector3.tscn");
-		
-		var debugVector = scene.Instance<DebugVector3>();
-		debugVector.vector = initVec;
-		debugVector.label = label;
-		debugVector.includeValueInLabel = includeValueInLabel;
-
-		parent.AddChild(debugVector);
-
-		return debugVector;
-	}
-
-	public override void _Ready()
-	{
-		this.viewport = GetNode<DebugVector3Viewport>("Viewport");
-        this.spatial = GetNode<Spatial>("Spatial");
+    {
+        get
+        {
+            return this.vector.x;
+        }
+        set
+        {
+            this.vector.x = value;
+        }
     }
 
-	public override void _Process(float delta)
-	{
-		var cam = GetViewport().GetCamera();
+    // Gets and sets the vector.y property.
+    [Export]
+    public float vectorY
+    {
+        get
+        {
+            return this.vector.y;
+        }
+        set
+        {
+            this.vector.y = value;
+        }
+    }
 
-		var parentPos = this.GlobalTransform.origin;
+    // Gets and sets the vector.z property.
+	[Export]
+	public float vectorZ
+    {
+        get
+        {
+            return this.vector.z;
+        }
+        set
+        {
+            this.vector.z = value;
+        }
+    }
 
-        this.spatial.Transform = new Transform(Quat.Identity, Vector3.Zero);
-        var start = cam.UnprojectPosition(this.spatial.GlobalTransform.origin);
+    // Color in which vector body will be drawn.
+	[Export]
+    public Color bodyColor = new Color(1, 0, 0, 1);
 
-		this.spatial.Transform = new Transform(Quat.Identity, this.vector);
-		var end = cam.UnprojectPosition(this.spatial.GlobalTransform.origin);
+    // Color in which vector end "arrow" will be drawn.
+	[Export]
+    public Color arrowColor = new Color(0, 1, 0, 1);
 
-		this.viewport.points = new LinePoint[1]{ new LinePoint(start, end) };
- 
-		GD.Print("start=" + start + "\nend=" + end + "\n-----");
+    // If true then the parent's rotation will be ignored.
+	[Export]
+    public bool ignoreParentRotation = true;
 
-		// // Draw vector
-		// var scaledVector = this.vector * this.vectorScale;
+    // Used to help ignore parent rotation if ignoreParentRoation is enabled.
+    private Vector3 initialGlobalRotation;
 
-		// this.Clear();
-		// this.Begin(Mesh.PrimitiveType.Lines);
+    // The label node in the world.
+    private Label3D labelNode;
 
-		// // Draw body
-		// this.SetColor(this.bodyColor);
+    // If true the vector's value will be shown in the label.
+    [Export]
+    public bool includeValueInLabel = true;
 
-		// this.AddVertex(new Vector3(0, 0, 0));
+    // If greater than 0 the vector will be rounded to this number of decimal places when shown in the label.
+    [Export]
+    public int roundLabelValue = 2;
 
-		// var body = scaledVector * 0.9f;
-		// this.AddVertex(body);
+    // This number will be used to scale the vector so it can appear bigger or smaller on the screen. The vector value shown in the label will not be scaled by this. To indicate the vector is being scaled an astrix will be appended.
+    [Export]
+    public float vectorScale = 1;
 
-		// // Draw arrow.
-		// this.SetColor(this.arrowColor);
+    // Create a DebugVector3 scene and add it as a child of the parent node.
+    static public DebugVector3 AddNode(Node parent, Vector3 initVec, String label, bool ignoreParentRotation = true, bool includeValueInLabel = true)
+    {
+        var scene = ResourceLoader.Load<PackedScene>("res://scenes/debug/vector3/DebugVector3.tscn");
+		
+        var debugVector = scene.Instance<DebugVector3>();
+        debugVector.vector = initVec;
+        debugVector.label = label;
+        debugVector.ignoreParentRotation = ignoreParentRotation;
+        debugVector.includeValueInLabel = includeValueInLabel;
 
-		// var arrow = scaledVector * 0.1f;
-		// this.AddVertex(body);
-		// this.AddVertex(body + arrow);
+        parent.AddChild(debugVector);
 
-		// this.End();
+        return debugVector;
+    }
 
-		// // Draw label
-		// var labelText = this.label;
-		// var roundedVector = this.vector;
+    public override void _Ready()
+    {
+        this.initialGlobalRotation = this.GlobalRotation;
+        this.labelNode = GetNode<Label3D>("Label3D");
+    }
 
-		// if (this.roundLabelValue >= 0)
-		// {
-		//     roundedVector = new Vector3(
-		// 		(float)Math.Round(this.vector.x, this.roundLabelValue),
-		// 		(float)Math.Round(this.vector.y, this.roundLabelValue),
-		// 		(float)Math.Round(this.vector.z, this.roundLabelValue)
-		// 	);
-		// }
+    public override void _Process(float delta)
+    {
+        // Maybe ignore rotation
+        if (this.ignoreParentRotation)
+        {
+            this.GlobalRotation = this.initialGlobalRotation;
+        }
 
-		// if (this.includeValueInLabel)
-		// {
-		//     labelText += " " + roundedVector;
-		// }
+        // Draw vector
+        var scaledVector = this.vector * this.vectorScale;
+		
+        this.Clear();
+        this.Begin(Mesh.PrimitiveType.Lines);
 
-		// if (this.vectorScale != 1f)
-		// {
-		//     labelText += "*";
-		// }		
+        // Draw body
+        this.SetColor(this.bodyColor);
 
-		// this.labelNode.Text = labelText;
+        this.AddVertex(new Vector3(0, 0, 0));
 
-		// // Deal with label being off screen
-		// var cam = GetViewport().GetCamera();
+        var body = scaledVector * 0.9f;
+        this.AddVertex(body);
 
-		// var labelPosition = scaledVector;
-		// var labelUnprojected = cam.UnprojectPosition(labelPosition + this.GlobalTransform.origin);
-		// var viewportSize = cam.GetViewport().Size;
+        // Draw arrow.
+        this.SetColor(this.arrowColor);
 
-		// if (labelUnprojected.x < 0 || labelUnprojected.x > viewportSize.x || labelUnprojected.y < 0 || labelUnprojected.y > viewportSize.y)
-		// {
-		//     var onScreenUnprojected = this.limitVector2(labelUnprojected, Vector2.Zero, viewportSize);
-		//     var zPosition = cam.GlobalTransform.origin.DistanceTo(this.GlobalTransform.origin);
+        var arrow = scaledVector * 0.1f;
+        this.AddVertex(body);
+        this.AddVertex(body + arrow);
 
-		//     labelPosition = cam.ProjectPosition(onScreenUnprojected, zPosition) - this.GlobalTransform.origin;
-		//     GD.Print("scaledVector=" + scaledVector + " (vector=" + this.vector + ")\nlabelUnprojected=" + labelUnprojected + "\nviewportSize=" + viewportSize + "\nlimitedVector2=" + onScreenUnprojected + "\nzPosition=" + zPosition + "\nreprojected=" + labelPosition + "\nglobal origin=" + this.GlobalTransform.origin + "\n-----");
-		// }
+        this.End();
 
-		// // Set label position (Label will appear backwards unless flipped on z axis)
-		// this.labelNode.Transform = new Transform(new Quat(new Vector3(0, 1, 0), Mathf.Pi), labelPosition);
-	}
+        // Draw label
+        var labelText = this.label;
+		var roundedVector = this.vector;
 
-	private Vector2 limitVector2(Vector2 value, Vector2 min, Vector2 max)
-	{
-		var x = value.x;
-		var y = value.y;
+        if (this.roundLabelValue >= 0)
+        {
+            roundedVector = new Vector3(
+				(float)Math.Round(this.vector.x, this.roundLabelValue),
+				(float)Math.Round(this.vector.y, this.roundLabelValue),
+				(float)Math.Round(this.vector.z, this.roundLabelValue)
+			);
+        }
 
-		if (x < min.x)
-		{
-			x = min.x;
-		} else if (x > max.x)
-		{
-			x = max.x;
-		}
+        if (this.includeValueInLabel)
+        {
+            labelText += " " + roundedVector;
+        }
 
-		if (y < min.y)
-		{
-			y = min.y;
-		} else if (y > max.y)
-		{
-			y = max.y;
-		}
+        if (this.vectorScale != 1f)
+        {
+            labelText += "*";
+        }		
 
-		return new Vector2(x, y);
-	}
+		this.labelNode.Text = labelText;
+
+		// Deal with label being off screen
+        var cam = GetViewport().GetCamera();
+
+        var labelPosition = scaledVector;
+        var labelUnprojected = cam.UnprojectPosition(labelPosition + this.GlobalTransform.origin);
+        var viewportSize = cam.GetViewport().Size;
+
+        if (labelUnprojected.x < 0 || labelUnprojected.x > viewportSize.x || labelUnprojected.y < 0 || labelUnprojected.y > viewportSize.y)
+        {
+            var onScreenUnprojected = this.limitVector2(labelUnprojected, Vector2.Zero, viewportSize);
+            var zPosition = cam.GlobalTransform.origin.DistanceTo(this.GlobalTransform.origin);
+
+            labelPosition = cam.ProjectPosition(onScreenUnprojected, zPosition) - this.GlobalTransform.origin;
+            GD.Print("scaledVector=" + scaledVector + " (vector=" + this.vector + ")\nlabelUnprojected=" + labelUnprojected + "\nviewportSize=" + viewportSize + "\nlimitedVector2=" + onScreenUnprojected + "\nzPosition=" + zPosition + "\nreprojected=" + labelPosition + "\nglobal origin=" + this.GlobalTransform.origin + "\n-----");
+        }
+
+		// Set label position (Label will appear backwards unless flipped on z axis)
+        this.labelNode.Transform = new Transform(new Quat(new Vector3(0, 1, 0), Mathf.Pi), labelPosition);
+    }
+
+    private Vector2 limitVector2(Vector2 value, Vector2 min, Vector2 max)
+    {
+        var x = value.x;
+        var y = value.y;
+
+        if (x < min.x)
+        {
+            x = min.x;
+        } else if (x > max.x)
+        {
+            x = max.x;
+        }
+
+        if (y < min.y)
+        {
+            y = min.y;
+        } else if (y > max.y)
+        {
+            y = max.y;
+        }
+
+        return new Vector2(x, y);
+    }
 }
